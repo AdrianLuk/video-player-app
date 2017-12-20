39c06b21f9a97391ce76de9d1f61e17dcd1f3844
Adrian <lukabaseball@hotmail.com>
2017-12-20 01:00:02 -0500

    adding videolist to app

---
+++ src/components/video_list.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const VideoList = () => {
+    return (
+        <ul className="col-lg-4 list-group">
+
+        </ul>
+    );
+};
+
+export default VideoList;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyCSocdS3SBE21vi7PPqhkYSPbVXVIjCPVw';
 
-YTSearch({ key: API_KEY, term: 'puppies' }, function (data) {
-    console.log(data);
-});
+
 //Create a new component. This component should produce some HTML
 class App extends Component {
     constructor(props) {
@@ -15,6 +13,10 @@ class App extends Component {
         this.state = {
             videos: []
         };
+        YTSearch({ key: API_KEY, term: 'puppies' }, (videos) => {
+            this.setState({ videos });
+            // this.setState({ videos: videos });
+        });
     }
 
     render() {
